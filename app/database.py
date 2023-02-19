a549c7b7c26fa2861e8c270e5b9f440d2915c908
Krapivin Gleb Olegovich <gkrapivin@ozon.ru>
2023-02-19 19:13:35 +0600

    test

--- app/database.py
+++ app/database.py
@@ -3,7 +3,12 @@ from sqlalchemy.ext.declarative import as_declarative, declared_attr
 from sqlalchemy.orm import sessionmaker
 
 from app.core.config import settings
-engine = create_engine(settings.DB_URI, pool_pre_ping=True, connect_args={"check_same_thread": False})
+
+ca = {}
+if "sqlite" in settings.DB_URI:
+    ca = {"check_same_thread": False}
+
+engine = create_engine(settings.DB_URI, pool_pre_ping=True, connect_args=ca)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 
@@ -13,4 +18,3 @@ class Base:
     @declared_attr
     def __tablename__(cls) -> str:
         return cls.__name__.lower()
-
